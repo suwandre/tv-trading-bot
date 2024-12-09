7eee13b7cf55031f1c9ac4cfd0c4688796e28876
Suwandre <suwandresukijat@gmail.com>
2024-12-09 20:16:00 +0100

    rf: updated deploy.sh contents

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-# Used to check if the app is running in the correct directory (in Railway)
+# Print the working directory
 echo "Current working directory: $(pwd)"
-echo "Contents of /app/target/release:"
-ls -l /app/target/release
-echo "Starting the app..."
+
+# List all directories in /app
+echo "Contents of /app:"
+ls -l /app
+
+# List all directories recursively
+echo "Complete directory tree:"
+find /app
+
+# Attempt to run the binary
+echo "Trying to start the app..."
 /app/target/release/tv-trading-bot
