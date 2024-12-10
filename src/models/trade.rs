013339d6a22257ed29ecd500c646c47f639330e2
Suwandre <suwandresukijat@gmail.com>
2024-12-10 16:31:33 +0100

    rf: trade signal enum now uses lowercase

--- src/models/trade.rs
+++ src/models/trade.rs
@@ -2,6 +2,7 @@ use serde::Deserialize;
 
 /// Used to determine a buy or sell signal.
 #[derive(Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "lowercase")]
 pub enum TradeSignal {
     Buy,
     Sell
