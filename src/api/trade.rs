6b7af0537cb8e23cb75452f907c68dd486e1e2a7
Suwandre <suwandresukijat@gmail.com>
2024-12-11 18:33:13 +0100

    rf: converted execute_trade to execute_paper_trade

--- src/api/trade.rs
+++ src/api/trade.rs
@@ -4,8 +4,10 @@ use serde_json::Value;
 
 use crate::models::{tradingview::TradingViewAlert, ApiResponse, TradeSignal};
 
-/// Executes a trade based on the alert received from TradingView.
-pub async fn execute_trade(payload: Json<Value>) -> (StatusCode, Json<ApiResponse<()>>) {
+/// Executes a paper trade based on the alert received from TradingView.
+/// 
+/// A paper trade will NOT use real money and will only be used for the purpose of recording/testing trades.
+pub async fn execute_paper_trade(payload: Json<Value>) -> (StatusCode, Json<ApiResponse<()>>) {
     println!("Received payload: {:?}", payload);
 
     match serde_json::from_value::<TradingViewAlert>(payload.0) {
@@ -13,6 +15,8 @@ pub async fn execute_trade(payload: Json<Value>) -> (StatusCode, Json<ApiRespons
             let expected_secret = std::env::var("TRADINGVIEW_SECRET").expect("TRADINGVIEW_SECRET must be set");
 
             if alert.secret != expected_secret {
+                eprintln!("Invalid secret provided.");
+
                 return (
                     StatusCode::UNAUTHORIZED,
                     Json(ApiResponse {
--- src/routes/trade.rs
+++ src/routes/trade.rs
@@ -1,7 +1,7 @@
 use axum::{routing::post, Router};
 
-use crate::api::trade::execute_trade;
+use crate::api::trade::execute_paper_trade;
 
 pub fn trade_routes() -> Router {
-    Router::new().route("/execute_trade", post(execute_trade))
+    Router::new().route("/execute_paper_trade", post(execute_paper_trade))
 }
